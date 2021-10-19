a7fb2b0eac45ad59148885f061fe33db65c8fddc
Dimas01u <89122911+Dimas01u@users.noreply.github.com>
2021-10-20 00:15:56 +0300

    my 1 project on javascript

---
+++ app.js
@@ -0,0 +1,23 @@
+function slidesPlugin(activeSlide) {
+	const slides = document.querySelectorAll('.slide')
+
+slides[activeSlide].classList.add('active')
+
+for (const slide of slides) {
+	slide.addEventListener('click', () => {
+		clearActiveClasses()
+
+		slide.classList.add('active')
+	})
+}
+
+function clearActiveClasses() {
+	
+	slides.forEach((slide) => {
+
+		slide.classList.remove('active')
+	})
+}
+}
+
+slidesPlugin(1)
